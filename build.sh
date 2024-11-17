af0490e47ebf432a9933c2f972b6792b67ae16b1
Adam <me@adamthiede.com>
2024-11-17 10:00:58 -0600

    use upstream silverblue instead of ublue's (#1)
    * Update build.sh
    
    * Update Containerfile
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh

--- build.sh
+++ build.sh
@@ -7,33 +7,121 @@ RELEASE="$(rpm -E %fedora)"
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
 rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
-# remove some base packages
+rpm-ostree install rpmfusion-free-release-tainted
+
+rpm-ostree override remove \
+    ffmpeg-free \
+    libavcodec-free \
+    libavdevice-free \
+    libavfilter-free \
+    libavformat-free \
+    libavutil-free \
+    libpostproc-free \
+    libswresample-free \
+    libswscale-free \
+    --install ffmpeg
 
 rpm-ostree override remove \
 	firefox firefox-langpacks \
 	virtualbox-guest-additions \
 	nano nano-default-editor \
-	gnome-software plocate \
-	yelp \
+ 	plocate \
+	yelp gnome-tour \
 	--install vim-default-editor
 
 # install preferred packages
-rpm-ostree install mpv ffmpeg yt-dlp \
-	fzf git curl htop neovim vim tmux go \
-	wl-clipboard w3m aerc bemenu \
-	android-tools aria2 btop cargo rust curl dino discount \
-	fastfetch flashrom imv isync \
-	jq keepassxc mosh mousepad mpc ncmpcpp \
-	ncdu nethack newsboat nmap rsync seahorse \
-	gvfs-nfs virt-manager river \
-	NetworkManager-tui syncthing tailscale
-
-#### Example for enabling a System Unit File
-#systemctl enable podman.socket
+rpm-ostree install \
+  NetworkManager-tui \
+  adw-gtk3-theme \
+  aerc \
+  alsa-firmware \
+  android-tools \
+  apr \
+  apr-util \
+  aria2 \
+  btop \
+  cargo \
+  curl \
+  curl \
+  dino \
+  discount \
+  distrobox \
+  fastfetch \
+  ffmpegthumbnailer \
+  flashrom \
+  flatpak-spawn \
+  fuse \
+  fzf \
+  git \
+  gnome-epub-thumbnailer \
+  gnome-tweaks \
+  go \
+  google-noto-sans-balinese-fonts \
+  google-noto-sans-cjk-fonts \
+  google-noto-sans-javanese-fonts \
+  google-noto-sans-sundanese-fonts \
+  grub2-tools-extra \
+  gvfs-nfs \
+  gvfs-nfs \
+  heif-pixbuf-loader \
+  htop \
+  imv \
+  intel-vaapi-driver \
+  isync \
+  jq \
+  just \
+  keepassxc \
+  kernel-tools \
+  libcamera \
+  libcamera-gstreamer \
+  libcamera-ipa \
+  libcamera-tools \
+  libdvdcss \
+  libratbag-ratbagd \
+  libva-utils \
+  lshw \
+  mosh \
+  mousepad \
+  mpc \
+  mpv \
+  ncdu \
+  ncmpcpp \
+  neovim \
+  net-tools \
+  nethack \
+  newsboat \
+  nmap \
+  nvme-cli \
+  nvtop \
+  openrgb-udev-rules \
+  openssl \
+  pam-u2f \
+  pam_yubico \
+  pamu2fcfg \
+  pipewire-plugin-libcamera \
+  powerstat \
+  river \
+  rsync \
+  rust \
+  seahorse \
+  smartmontools \
+  squashfs-tools \
+  symlinks \
+  syncthing \
+  tailscale \
+  tcpdump \
+  tmux \
+  tmux \
+  traceroute \
+  vim \
+  vim \
+  virt-manager \
+  w3m \
+  wireguard-tools \
+  wl-clipboard \
+  wl-clipboard \
+  yt-dlp \
+  yubikey-manager \
+  zstd
