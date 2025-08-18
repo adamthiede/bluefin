e337cb4fa15fda1237c8afb9afdc71e3e7edd080
adamthiede <me@adamthiede.com>
2025-08-18 18:46:20 -0500

    persistent notification with a button

--- ostree-notify/ostree-notify.sh
+++ ostree-notify/ostree-notify.sh
@@ -2,6 +2,5 @@
 rpm-ostree status --pending-exit-77
 ec=$?
 if [[ $ec == 77 ]];then
-	# 60 second persistent notification
-	notify-send -t 60000 "System Updates Notification" "Updates have been applied, and a reboot is required to use the updated system."
+	notify-send -A Close=Close "System Updates Notification" "Updates have been applied, and a reboot is required to use the updated system."
 fi
