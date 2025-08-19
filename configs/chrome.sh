5ffe1149df5ac8401fb2b113b1e3123efa94b484
adamthiede <me@adamthiede.com>
2025-08-19 09:04:09 -0500

    more ln

--- configs/chrome.sh
+++ configs/chrome.sh
@@ -3,6 +3,7 @@ echo "$1"
 if [[ "$1" == "chrome" ]];then
     echo "installing chrome"
     mkdir -p /usr/lib/opt/google /var/opt
+    ln -s "/var/opt"  "/opt"
     ln -s /usr/lib/opt/google /var/opt/google
     sed -e 's,enabled=0,enabled=1,' -i /etc/yum.repos.d/google-chrome.repo
     rpm-ostree override remove firefox firefox-langpacks --install google-chrome-stable
