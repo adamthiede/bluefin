217ddf8470cd14ca16fe8acfcc96a893696a49cf
adamthiede <me@adamthiede.com>
2025-08-19 10:19:01 -0500

    update script

--- configs/chrome.sh
+++ configs/chrome.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
+set -x
 echo "$1"
 if [[ "$1" == "chrome" ]];then
     echo "--- INSTALLING CHROME"
@@ -6,7 +8,11 @@ if [[ "$1" == "chrome" ]];then
     ln -s /var/opt  /opt
     ln -s /usr/lib/opt/google /var/opt/google
     sed -e 's,enabled=0,enabled=1,' -i /etc/yum.repos.d/google-chrome.repo
-    rpm-ostree override remove firefox firefox-langpacks --install google-chrome-stable
+    sleep 5
+    rpm-ostree override remove firefox firefox-langpacks
+    sleep 5
+    rpm-ostree install google-chrome-stable
+    sleep 5
 else
     echo "--- NOT INSTALLING CHROME"
 fi
